489652e0a8eb0dbabccf835e5c95fe97c14b42ba
Enrique Lopez <enrique@guatemala.com>
2020-09-11 14:08:12 -0600

    form

---
+++ components/Form.js
@@ -0,0 +1,86 @@
+import React, {useState} from 'react';
+import {View, TextInput, StyleSheet, TouchableWithoutFeedback, Text, Animated} from 'react-native';
+import {Picker} from '@react-native-community/picker';
+
+const Form = () => {
+    const [animationBtn] = useState(new Animated.Value(1));
+
+    const styleAnimate = {
+        transform: [{scale: animationBtn}]
+    }
+
+    const animationInput = () => {
+        Animated.spring(animationBtn, {
+            toValue: .75
+        }).start();
+    }
+
+    const animationOutput = () => {
+        Animated.spring(animationBtn, {
+            toValue: 1,
+            friction: 4,
+            tension: 30
+        }).start();
+    }
+
+
+    return (
+        <>
+            <View style={styles.form}>
+                <View>
+                    <TextInput
+                        style={styles.input}
+                        placeholder='city'
+                        placeholderTextColor='#666'
+                    />
+                </View>
+                <View>
+                    <Picker
+                        itemStyle={{height: 120, backgroundColor: '#fff'}}>
+                        <Picker.Item label='Select country' value=''/>
+                        <Picker.Item label='Estados unidos' value='US'/>
+                        <Picker.Item label='Mexico' value='MX'/>
+                        <Picker.Item label='Argentina' value='AR'/>
+                        <Picker.Item label='Colombia' value='CO'/>
+                        <Picker.Item label='España' value='ES'/>
+                    </Picker>
+                </View>
+                <TouchableWithoutFeedback
+                    onPressIn={() => animationInput()}
+                    onPressOut={() => animationOutput()}
+                >
+                    <Animated.View
+                        style={[styles.btnSearch, styleAnimate]}>
+                        <Text style={styles.txtSearch}>Search Weather</Text>
+                    </Animated.View>
+                </TouchableWithoutFeedback>
+            </View>
+        </>
+    )
+};
+
+const styles = StyleSheet.create({
+    input: {
+        padding: 10,
+        height: 50,
+        backgroundColor: '#fff',
+        fontSize: 20,
+        marginBottom: 20,
+        textAlign: 'center'
+    },
+    btnSearch: {
+        marginTop: 50,
+        backgroundColor: '#000',
+        padding: 10,
+        justifyContent: 'center'
+    },
+    txtSearch: {
+        color: '#fff',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        textAlign: 'center',
+        fontSize: 18
+    }
+});
+
+export default Form;
