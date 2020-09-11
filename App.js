fe8795b66aeb80d2e34db2ad22c247cd83211965
Enrique Lopez <enrique@guatemala.com>
2020-09-11 14:08:42 -0600

    animated and form

--- App.js
+++ App.js
@@ -4,21 +4,40 @@ import {
     StyleSheet,
     ScrollView,
     View,
-    Text,
-    StatusBar,
+    Keyboard,
+    TouchableWithoutFeedback,
 } from 'react-native';
-
+import Form from './components/Form';
 
 const App = () => {
+
+    const hideKeyboard = () => {
+        Keyboard.dismiss();
+    }
+
+
     return (
         <>
-
+            <TouchableWithoutFeedback onPress={() => hideKeyboard()}>
+                <View style={styles.app}>
+                    <View style={styles.container}>
+                        <Form/>
+                    </View>
+                </View>
+            </TouchableWithoutFeedback>
         </>
     );
 };
 
 const styles = StyleSheet.create({
-
+    app: {
+        flex: 1,
+        backgroundColor: 'rgb(71, 149, 212)',
+        justifyContent: 'center'
+    },
+    container: {
+        marginHorizontal: '2.5%'
+    }
 });
 
 export default App;
